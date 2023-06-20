ea686a85ba07682818e9f50a392e1cbb98c0cd7e
abdelbasset4 <benatmani1999@gmail.com>
2023-06-20 22:12:12 +0200

    switch project to use the last react router dom 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-import { Fragment} from "react"
-import Navbar from "./components/Navbar"
 import MoviesList from "./components/MoviesList"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter,createRoutesFromElements,RouterProvider, Route } from 'react-router-dom'
 import Details from "./components/Details"
+import RootLayout from "./layouts/RootLayout"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route  index element={ <MoviesList/> } />
+      <Route  path="/movie/:id" element={ <Details/> } />
+      
+    </Route>
+  )
+)
 
 
 function App() {
-  
-
-
-  
-
-  
-
   return (
-
-     <Fragment>
-      <Navbar/>
-      
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MoviesList/>}/>
-          <Route path="/movie/:id" element={<Details />} />
-        </Routes>
-      </BrowserRouter>
-     </Fragment>
+    <RouterProvider router={router} />
   )
 }
 
